a078d565989bf36a7a2cc0f957a302519a7f9ad4
LizethG0219 <lizethgaray.0219@gmail.com>
2020-02-21 20:53:13 -0800

    h

---
+++ Pelicula/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Pelicula
+{
+    class pelicula 
+    {
+        //Creacion de los atributos
+        public string titulo;
+        public int16 año;
+        public string pais;
+        public string director;
+        
+         //Creacion de los metodos 
+         public pelicula (string tit)
+         {
+             this.titulo = tit;
+         }
+         public int16 setAño ( int16 a)
+         {
+            this.
+         }
+
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creacion de los objetos
+            pelicula p1 = new pelicula();
+            pelicula p2 = new pelicula();
+            Console.WriteLine();
+        }
+    }
+}
