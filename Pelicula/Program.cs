ce9e8ecc3e565b06678f73c1d603a082ff8a0cad
LizethG0219 <lizethgaray.0219@gmail.com>
2020-02-21 23:28:14 -0800

    ya

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -1,14 +1,14 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace Pelicula
 {
     class pelicula 
     {
         //Creacion de los atributos
-        public string titulo;
-        public int16 año;
-        public string pais;
-        public string director;
+        private string titulo;
+        private int16 año;
+        private string pais;
+        private string director;
         
          //Creacion de los metodos 
          public void setTitulo (string tit)
@@ -53,23 +53,28 @@ namespace Pelicula
         static void Main(string[] args)
         {
             //Creacion de los objetos
-            pelicula p1 = new pelicula();
-            p1.setTitulo("La forma del agua");
-            p1.setAño(2018);
-            p1.titulo ="La forma del agua";
-            p1.año = 2018;
-            p1.pais = "Estados Unidos";
-            p1.director = "Guillermo del Toro";
-            Console.WriteLine("{0}{1}"), "La pelicula es: " + p1.titulo + "El año es: " + p1.año;
+            pelicula p1 = new pelicula("La pelicula ganadora: La forma del agua", 2018);
+            //p1.setTitulo("La forma del agua");
+            //p1.setAño(2018);
+            //p1.titulo ="La forma del agua";
+            //p1.año = 2018;
+            //p1.pais = "Estados Unidos";
+            //p1.director = "Guillermo del Toro";
+            //Console.WriteLine("{0}{1}"), "La pelicula es: " + p1.titulo + "El año es: " + p1.año);
+            p1.Imprime();
 
-            pelicula p2 = new pelicula();
-            p2.setTitulo("La la land");
-            p2.setAño(2016);
-            p2.titulo = "Estados Unidos";
-            p2.año = 2016;
-            p2.pais = "Estados Unidos";
-            p2.director = "Damien Chazelle";
-            Console.WriteLine("{0}{1}"), "La pelicula es: " + p2.titulo + "El año es: " + p2.año;;
+            Console.WriteLine();
+            Console.WriteLine();
+
+            pelicula p2 = new pelicula("La pelicula ganadora: La la land", 2016);
+           // p2.setTitulo("La la land");
+            //p2.setAño(2016);
+            //p2.titulo = "Estados Unidos";
+            //p2.año = 2016;
+            //p2.pais = "Estados Unidos";
+            //p2.director = "Damien Chazelle";
+            //Console.WriteLine("{0}{1}"), "La pelicula es: " + p2.titulo + "El año es: " + p2.año);
+            p2.Imprime();
         }
     }
 }
