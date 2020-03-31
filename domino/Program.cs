40646679d889db64c1be04c7be066cd601fc6a8f
LizethG0219 <lizethgaray.0219@gmail.com>
2020-03-31 15:35:27 -0700

    lis

---
+++ domino/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace domino
+{
+    class Domino
+    {
+        public int a;
+        public int b;
+
+        public Domino(int A, int B)
+        {
+            a = A;
+            b = B;
+        }
+        public static int operator +(Domino dom1, Domino dom2)
+        {
+            return (dom1.a + dom2.a + dom1.b + dom2.b);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Domino A = new Domino(2, 0);
+            Domino B = new domino (5, 3);
+            Console.WriteLine("La suma del domino es: "( A+ B));
+        }
+    }
+}
